7785946a10df3a7f2a1e9d98d7ae815d88c60671
Kyle Bloom <kyle@symless.com>
2020-09-15 21:59:57 +0100

    Add self test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { promisify } = require('util');
-const appendFile = promisify(require('fs').appendFile);
-const core = require('@actions/core');
+const { promisify } = require(`util`);
+const appendFile = promisify(require(`fs`).appendFile);
+const core = require(`@actions/core`);
 
 const run = async () => {
-  const token = core.getInput('token');
+  const token = core.getInput(`token`);
 
   try {
-    await appendFile('.npmrc', `//npm.pkg.github.com/:_authToken=${token}`);
+    await appendFile(`.npmrc`, `//npm.pkg.github.com/:_authToken=${token}`);
   } catch (error) {
     core.setFailed(error.message);
   }
